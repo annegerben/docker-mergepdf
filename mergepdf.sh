abb1871d6780b2afa74ce822125c7c43ed8d92e8
annegerben <annegerben@users.noreply.github.com>
2020-07-25 17:35:22 +0200

    Update mergepdf.sh

--- mergepdf.sh
+++ mergepdf.sh
@@ -12,7 +12,7 @@ fi
 
 if [[ $1 == *.jpg || $1 == *.tiff ]]; then
   echo "got a picture, copy direct to output"
-  inotifywait -t FW_TIMEOUT -e close $1
+  inotifywait -t 600 -e close $1
   mv $1 $OUTPUT/"$(date +%Y-%m-%d_%H-%M-%S)_"$1
   echo "done..."
   exit
@@ -26,7 +26,7 @@ fi
 if [[  "$1" != odd*.pdf && "$1" != even*.pdf  ]]; then
   #no multipage pdf file, copy directly to Output folder
   echo "$(date +%Y-%m-%d_%H-%M-%S) - detected non mutlipage file"
-  inotifywait -t FW_TIMEOUT -e close $1
+  inotifywait -t 600 -e close $1
   sleep 3
   rm $1
   echo "$(date +%Y-%m-%d_%H-%M-%S) OCR done!"
@@ -37,23 +37,24 @@ fi
 if [[ "$1" == odd*.pdf ]]; then
   echo "$(date +%Y-%m-%d_%H-%M-%S) - odd file detected"
   mv -f $1 $stringOdd
-else
+fi
+
+if [[ "$1" == even*.pdf ]]; then
   echo "$(date +%Y-%m-%d_%H-%M-%S) - even file detected"
   mv -f $1 $stringEven
 fi
 
 if [[ -f $stringOdd && -f $stringEven ]]; then
   stringMerged="$(date +%Y-%m-%d_%H-%M-%S)_merged.pdf"
-  inotifywait -t FW_TIMEOUT -e close $stringOdd $stringEven
+  inotifywait -t 600 -e close $stringOdd $stringEven
   sleep 1
   echo "executing pdftk"
-  pdftk A=$stringOdd B=$stringEven shuffle A Bend-1 output $stringMerged
+  pdftk A=$stringOdd B=$stringEven shuffle A Bend-1 output $OUTPUT/$stringMerged
   if [[ $RET -eq 0 ]]; then
     echo "pdftk was successful, removing temporary files..."
     echo "merged file: $stringMerged"
     rm  -f $stringOdd $stringEven
   fi
-  rm $stringMerged
   sleep 1
   echo "done..."
   exit
