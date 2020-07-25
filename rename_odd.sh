6d0e0ab10b479e207f39cc18e8bebc41dc26d661
annegerben <annegerben@users.noreply.github.com>
2020-07-25 17:36:57 +0200

    Update rename_odd.sh

--- rename_odd.sh
+++ rename_odd.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-cd /srv/input/odd
-mv "$2" "/srv/input/document_o.pdf"
+if [[  $1 == *_merged.pdf || $1 == *_o.pdf || $1 == *_e.pdf  ]]; then
+  exit
+fi
+
+cd /srv/odd
+
+if [[ $1 == *.pdf ]]; then
+  sleep 5
+  mv $1 /srv/input/"odd_"$1
+  exit
+fi
